7daea93fd60fd00acddd9b25b0d6ee15fb0b7e58
Varun Keerthy <vkeerthy@btes.com.au>
2018-07-22 20:27:54 +1000

    Feature: Lecture 13: State changes in activity

--- app/src/main/java/tutorial/demo/btes/com/biztechtutorialapp/MainActivity.java
+++ app/src/main/java/tutorial/demo/btes/com/biztechtutorialapp/MainActivity.java
@@ -1,26 +1,66 @@
 package tutorial.demo.btes.com.biztechtutorialapp;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
     }
 
-    public void buttonClicked(View v){
-        /*TextView textViewResult = (TextView) findViewById(R.id.userBMI);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    /* public void buttonClicked(View v){
+       TextView textViewResult = (TextView) findViewById(R.id.userBMI);
         EditText editTextHeight = (EditText) findViewById(R.id.userHeight);
         EditText editTextWeight = (EditText) findViewById(R.id.userWeight);
         double userHeight = Double.parseDouble(editTextHeight.getText().toString());
         double userWeight = Double.parseDouble(editTextWeight.getText().toString());
         double bmi = userWeight / (userHeight * userHeight);
-        textViewResult.setText(Double.toString(bmi));*/
-    }
+        textViewResult.setText(Double.toString(bmi));
+    }*/
 }
