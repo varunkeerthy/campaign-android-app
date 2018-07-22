5f13f34fb3e8ce0323cc1eed890f36bd12b4b8b5
Varun Keerthy <vkeerthy@btes.com.au>
2018-07-22 21:31:28 +1000

    Feature: Lecture 17: Using onClickListener method on UI buttons.

--- app/src/main/java/tutorial/demo/btes/com/biztechtutorialapp/MainActivity.java
+++ app/src/main/java/tutorial/demo/btes/com/biztechtutorialapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,13 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TextView textView = (TextView) findViewById(R.id.text);
+                        textView.setText("button clicked");
+                    }
+                }
+        );
     }
 
-    public void buttonClicked(View v) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-
-    }
 
     @Override
     protected void onStart() {
